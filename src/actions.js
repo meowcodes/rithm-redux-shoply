10642b3d0b57ebec8f1f427e2f834d8b617009e8
Chao Ye <Chao12.ye@gmail.com>
2019-04-02 11:58:17 -0700

    add action.js

---
+++ src/actions.js
@@ -0,0 +1,16 @@
+import { ADD, REMOVE } from './actionTypes';
+
+
+export function add(id) {
+    return {
+        type: ADD,
+        payload: { id }
+    };
+}
+
+export function remove(id) {
+    return {
+        type: REMOVE,
+        payload: { id }
+    };
+}
