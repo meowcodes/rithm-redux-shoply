9cfaa2c16c5917a423179c45ba2d4f7531af1895
meowcodes <meowcodes@gmail.com>
2019-04-02 14:26:40 -0700

    update data structure of Redux state

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -1,26 +1,29 @@
 import { ADD, REMOVE } from './actionTypes';
-const INITIAL_STATE = { cart: [] };
+const INITIAL_STATE = { cart: {} };
 
 
 function rootReducer(state = INITIAL_STATE, action) {
   console.log("IN REDUCER", state, action);
+  const currId = action.payload ? action.payload.id + "" : null;
 
   switch (action.type) {
     case ADD:
-      let addItem = [...state.cart, action.payload.id];
-
-      return { cart: addItem };
-
-    case REMOVE:
-      let itemIndex = state.cart.indexOf(action.payload.id);
-      if(itemIndex !== -1){
-        let removeItem = [
-          ...state.cart.slice(0, itemIndex),
-          ...state.cart.slice(itemIndex + 1)
-        ];
-        return { cart: removeItem };
+      let currCount = 0;
+      
+      if(state.cart[currId]){
+        currCount = state.cart[currId]
       }
 
+      return { cart: {...state.cart, [currId]: currCount + 1 }}
+    case REMOVE:
+      if(state.cart[currId] > 1){
+        let currCount = state.cart[currId]
+        return { cart: {...state.cart, [currId]: currCount - 1 }}
+      }else {
+        let currCart = state.cart;
+        delete currCart[currId];
+        return { cart: currCart};
+      }
     default:
       return state;
   }
