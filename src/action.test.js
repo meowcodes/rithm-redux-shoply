736064bc88c6f268e1146e8d1ad3001bfc40fdaf
Chao Ye <Chao12.ye@gmail.com>
2019-04-02 15:54:50 -0700

    add unit test for action creators

---
+++ src/action.test.js
@@ -0,0 +1,23 @@
+import { ADD, REMOVE } from "./actionTypes";
+import { add, remove } from "./actions";
+
+describe("action creator add", function() {
+  it("return expected action", function() {
+    let action = add(2);
+    expect(action).toEqual({
+      type: ADD,
+      payload: { id: 2 }
+    });
+  });
+});
+
+
+describe("action creator remove", function() {
+    it("return expected action", function() {
+      let action = remove(2);
+      expect(action).toEqual({
+        type: REMOVE,
+        payload: { id: 2 }
+      });
+    });
+  });
